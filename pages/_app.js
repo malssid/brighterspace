378934e3d5588d6c27962d482caab2ec13abe374
michaelalssid <54999561+michaelalssid@users.noreply.github.com>
2021-04-01 18:25:43 -0400

    added theme for background color

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
-import { Box, Image, ChakraProvider } from "@chakra-ui/react";
+import { Box, Image, ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#c0ddf2",
+      },
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Box bg="#002147" w="100%" p={4}>
-        <Image src="brighterspace.svg" alt="logo" />
+        <Image src="brighterspace.svg" alt="logo" w="20rem"/>
       </Box>
       <Component {...pageProps} />
     </ChakraProvider>
