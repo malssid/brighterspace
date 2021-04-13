4fb4b465d5c81d30e4360585faee0e9b23010d20
Benjamin Dahrooge <bdahrooge@uri.edu>
2021-04-13 19:12:41 -0400

    Add login and login test

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,134 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+import { db, query } from "../../../lib/db"
+
+import shajs from "sha.js"
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
+  // https://next-auth.js.org/configuration/providers
+  providers: [
+    // Providers.Email({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    // }),
+
+    Providers.Credentials({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: 'Brigherspace Credentials',
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Email", type: "text", placeholder: "rhody@uri.edu" },
+        password: {  label: "Password", type: "password" }
+      },
+     async authorize(credentials) {
+
+        credentials.password = shajs('sha256').update(credentials.password).digest('hex')
+
+        console.log(credentials.password)
+
+        const results = await query("SELECT * FROM people WHERE Email = ? AND Password = ? LIMIT 1", [
+          credentials.username,
+          credentials.password
+        ])
+
+        console.log("Logged in user")
+        console.log(results)
+
+        if(results.length === 0){
+          return false
+        }
+        return {
+          id: results[0].pid,
+          name: results[0].First_name + " " + results[0].Last_name,
+          email: results[0].Email
+        }
+      
+      }
+    })
+  
+  ],
+  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
+  // https://next-auth.js.org/configuration/databases
+  //
+  // Notes:
+  // * You must install an appropriate node_module for your database
+  // * The Email provider requires a database (OAuth providers do not)
+  // database: {
+  //   type: 'mysql',
+  //   port: 3306,
+  //   host: process.env.MYSQL_HOST,
+  //   database: process.env.MYSQL_DATABASE,
+  //   username: process.env.MYSQL_USERNAME,
+  //   password: process.env.MYSQL_PASSWORD,
+  // },
+
+  // The secret should be set to a reasonably long random string.
+  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+  // a separate secret is defined explicitly for encrypting the JWT.
+  // secret: process.env.SECRET,
+  secret: "seceret",
+  
+
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    // This option can be used with or without a database for users/accounts.
+    // Note: `jwt` is automatically set to `true` if no database is specified.
+    jwt: true,
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    // updateAge: 24 * 60 * 60, // 24 hours
+  },
+
+  // JSON Web tokens are only used for sessions if the `jwt: true` session
+  // option is set - or by default if no database is specified.
+  // https://next-auth.js.org/configuration/options#jwt
+  jwt: {
+    // A secret to use for key generation (you should set this explicitly)
+    secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',
+    // Set to true to use encryption (default: false)
+    // encryption: true,
+    // You can define your own encode/decode functions for signing and encryption
+    // if you want to override the default behaviour.
+    // encode: async ({ secret, token, maxAge }) => {},
+    // decode: async ({ secret, token, maxAge }) => {},
+  },
+
+  // You can define custom pages to override the built-in ones. These will be regular Next.js pages
+  // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
+  // The routes shown here are the default URLs that will be used when a custom
+  // pages is not specified for that route.
+  // https://next-auth.js.org/configuration/pages
+  pages: {
+    // signIn: '/auth/signin',  // Displays signin buttons
+    // signOut: '/auth/signout', // Displays form with sign out button
+    // error: '/auth/error', // Error code passed in query string as ?error=
+    // verifyRequest: '/auth/verify-request', // Used for check email page
+    // newUser: null // If set, new users will be directed here on first sign in
+  },
+
+  // Callbacks are asynchronous functions you can use to control what happens
+  // when an action is performed.
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    // async signIn(user, account, profile) { return true },
+    // async redirect(url, baseUrl) { return baseUrl },
+    // async session(session, user) { return session },
+    // async jwt(token, user, account, profile, isNewUser) { return token }
+  },
+
+  // Events are useful for logging
+  // https://next-auth.js.org/configuration/events
+  events: {},
+
+  // Enable debug messages in the console if you are having problems
+  debug: true,
+})
