ed93517fecbdbbff8005b279b15a1409012fd62d
malssid <mlalssid@gmail.com>
2021-04-15 16:20:01 -0400

    Made course cards clickable and each course page now handles someone not having access to a course

--- components/Announcement.js
+++ components/Announcement.js
@@ -1,10 +1,19 @@
-import { Box, Badge, Text } from '@chakra-ui/react'
+import { Box, Badge, Text } from "@chakra-ui/react";
 
-export default function Announcement({data}) {
-    return (
-        <Box>
-            <Badge variant="subtle">Posted on Jan 1</Badge>
-            <Text>{data.body}</Text>
-        </Box>
-    )
+export default function Announcement({ data }) {
+  return (
+    <Box
+      borderRadius="lg"
+      boxShadow="lg"
+      bg="blue.100"
+      w="80"
+      p={4}
+      color="blue.900"
+      mb="4"
+      cursor="pointer"
+    >
+      <Text>{data.body}</Text>
+      <Badge mt={2} borderRadius="full" py={1} px={2} bg="blue.200">Posted on Jan 1</Badge>
+    </Box>
+  );
 }
--- components/CourseCard.js
+++ components/CourseCard.js
@@ -1,21 +1,25 @@
 import { Box, Heading, Text, Badge } from "@chakra-ui/react";
+import Link from "next/link";
 
-export default function CourseCard({ name, desc, term }) {
+export default function CourseCard({ name, cid, desc, term }) {
   return (
-    <Box
-      borderRadius="lg"
-      boxShadow="lg"
-      bg="blue.50"
-      w="50"
-      p={4}
-      color="blue.900"
-      mb="4"
-    >
-      <Heading>{name}</Heading>
-      <Text>{desc}</Text>
-      <Badge borderRadius="full" py={1} px={2} colorScheme="blue">
-        {term}
-      </Badge>
-    </Box>
+    <Link href={`/course/${cid}`}>
+      <Box
+        borderRadius="lg"
+        boxShadow="lg"
+        bg="blue.100"
+        w="50"
+        p={4}
+        color="blue.900"
+        mb="4"
+        cursor="pointer"
+      >
+        <Heading>{name}</Heading>
+        <Text>{desc}</Text>
+        <Badge borderRadius="full" py={1} px={2} bg="blue.200">
+          {term}
+        </Badge>
+      </Box>
+    </Link>
   );
 }
