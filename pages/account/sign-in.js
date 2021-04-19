4721a2d08df624a3aefa77ecd4514bfa77287e3a
malssid <mlalssid@gmail.com>
2021-04-19 19:55:31 -0400

    Redirects user from sign in to home page if already signed in

--- pages/account/sign-in.js
+++ pages/account/sign-in.js
@@ -1,24 +1,25 @@
-import LoginForm from "../../components/LoginForm"
-import { getCsrfToken } from 'next-auth/client'
-import { useRouter } from 'next/router'
-
+import LoginForm from "../../components/LoginForm";
+import { getCsrfToken, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function LoginPage(props) {
-    const router = useRouter()
+  const router = useRouter();
+  const [session, loading] = useSession();
 
-    return (
-        <LoginForm 
-            error={router.query.error}
-            csrfToken={props.csrfToken} 
-        />
-    )
+  useEffect(() => {
+    if (session) {
+      router.push("/");
+    }
+  }, [loading]);
 
+  return <LoginForm error={router.query.error} csrfToken={props.csrfToken} />;
 }
 
 export async function getServerSideProps(context) {
-    return {
-      props: {
-        csrfToken: await getCsrfToken(context)
-      }
-    }
-  }
+  return {
+    props: {
+      csrfToken: await getCsrfToken(context),
+    },
+  };
+}
