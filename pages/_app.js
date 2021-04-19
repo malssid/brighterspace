b96c294a4872433b43bdaa128cb8936f034ea3fb
malssid <mlalssid@gmail.com>
2021-04-19 18:53:33 -0400

    still trying to fix vercel errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,25 @@
-import "@fontsource/chivo/";
-import "@fontsource/Overpass/";
+import "@fontsource/chivo/700.css";
+import "@fontsource/Overpass/400.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 import { Provider } from "next-auth/client";
 import theme from "../styles/theme";
-import Head from "next/head"
+import NextHead from "next/head";
+import { GoogleFonts } from "next-google-fonts";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
   const [pageTitle, setPageTitle] = useState(undefined);
 
   return (
     <>
-      <Head>
+      <NextHead>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{pageTitle || "Brighterspace"}</title>
-      </Head>
+      </NextHead>
       <Provider session={pageProps.session}>
         <ChakraProvider theme={theme}>
           <Navbar pageTitle={pageTitle} />
