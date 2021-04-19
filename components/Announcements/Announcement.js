dfc334d4d93cf062fa49908a63ed7d867545be26
Benjamin Dahrooge <bdahrooge@uri.edu>
2021-04-18 23:36:06 -0400

    announcements can be posted by instructor
    Co-Authored-By: Michael Alssid <67285754+malssid@users.noreply.github.com>

---
+++ components/Announcements/Announcement.js
@@ -0,0 +1,119 @@
+
+import {
+  Flex, 
+  Badge, 
+  Text, 
+  Box, 
+  useDisclosure, 
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  Textarea,
+  Spacer,
+  Alert,  
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react"
+
+import {useState} from 'react'
+
+/**
+ * Displays an annocument card with title, poster, body and date posted
+ * @param {object} param0 Annocument data
+ * @returns 
+ */
+export function Announcement({ data }) {
+  return (
+    <Flex
+      borderRadius="lg"
+      boxShadow="lg"
+      bg="blue.100"
+      w="80"
+      p={4}
+      color="blue.900"
+      mb="4"
+      cursor="pointer"
+      justify="center"
+      align="center"
+    >
+      <Text>{data.body}</Text>
+      <Badge mt={2} borderRadius="full" py={1} px={2} bg="blue.200">{data.dateposted}</Badge>
+    </Flex>
+  );
+}
+
+/**
+ * 
+ * @param {props} props Should include a courseid and pid of the poster 
+ */
+export function NewAnnouncement({cid}) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const [posted, isPosted] = useState(false);
+  const [body, setBody] = useState("");
+
+  async function postAnnouncement(){
+    const result = await fetch('../../../api/announcements/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': "application/json"
+      },
+      body: JSON.stringify({cid, body})
+    })
+
+    console.log(result)
+
+    if(result.status === 200){
+      isPosted(true)
+      setTimeout(() => {
+        onClose();
+      }, 1000)
+    }else{
+      // Display some type of error
+    }
+  }
+
+  return (
+    <>
+      <Box pb="10px">
+        <Button onClick={onOpen}>New Announcement</Button>
+      </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered  motionPreset="slideInBottom">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>New Announcement</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {posted && <Alert status="success">
+              <AlertIcon />
+                Announcement posted
+            </Alert>}
+            <Textarea 
+              onChange={e => setBody(e.target.value)}
+              placeholder="Enter your announcement text here" />
+          </ModalBody>
+
+          <ModalFooter>
+            <Flex>
+            <Button variant="ghost" onClick={onClose} mr={3}>Cancel</Button>
+
+            <Spacer />
+            <Button colorScheme="blue" onClick={postAnnouncement}>
+                  Post Announcement
+                </Button>
+            </Flex>
+
+
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
---
+++ pages/api/announcements/new.js
@@ -0,0 +1,46 @@
+import { query } from "../../../lib/db";
+import { getSession } from 'next-auth/client'
+
+export default async function(req, res){
+    
+    const session = await getSession({ req })
+
+    if (session) {
+        // Signed in
+
+        // Determine if userid has privelges to post annocuments in the cid
+    
+        const memberships = await query("SELECT * FROM memberships WHERE pid = ? AND cid = ? LIMIT 1", 
+            [session.user.id, req.body.cid])
+
+        // console.log([session.user.id, req.body.cid])
+
+        const role = memberships[0]?.role ?? null;
+
+        // console.log(memberships)
+
+        if(role === 1){
+            const result = await query("INSERT INTO `announcements` (`aid`, `pid`, `cid`, `dateposted`, `title`, `body`) VALUES (NULL, ?, ?, CURRENT_TIMESTAMP, NULL, ?)", 
+                [session.user.id, req.body.cid, req.body.body])
+
+            if(result){
+                res.json({status: 200})
+            }else{
+                res.status(500);
+                res.json({status: 500, reason: "Unknown database error"})
+
+            }         
+
+        }else{
+            res.status(403)
+            res.json({status: 403, reason: "You do not have the correct permissions to perform this action"})            
+        }
+
+
+    } else {
+      // Not Signed in, user cannot create a new annoucment
+      res.status(401)
+    }
+    res.end()
+
+}
